88db48eb9652145077529a26d85a3ad8f46eaa95
John <jsnd11@gmail.com>
2023-07-30 22:30:51 -0400

    adding build

--- src/About.js
+++ src/About.js
@@ -2,7 +2,7 @@ export default function About(){
   return(
     <div class='about'>
       <h2 class = 'about__head'>About</h2>
-      <p class = 'about__info'>I am a full-stack developer with a commitment to building tools that contribute to the public good. I am happiest when I'm learning.</p>
+      <p class = 'about__info'>I am a full-stack developer with a commitment to building tools that contribute to the public good. I am happiest when I'm learning!</p>
     </div>
   )
 }
